7907ef3a785116b8320173027634d1c8ebc32744
Lilia Kotonii <l.kotonij@yahoo.com>
2019-10-06 22:03:32 +0300

    problem4

---
+++ problem4.py
@@ -0,0 +1,20 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+# returns True if reversed string is equal to the string received as parameter
+def isPalindrome(string):
+    tempList=list(string)
+    tempList.reverse()
+    if (''.join(tempList)==string):
+        return True
+
+# finds largest palindrome made from the product of numbers in a certain range
+def findLargestPalindrome(start, end, step):
+    palindrome=0
+    for i in range(start,end,step):
+        for a in range(start,end,step):
+            product=i*a
+            if(isPalindrome(str(product))):
+                palindrome=max(product,palindrome)
+    print(palindrome)
+
+findLargestPalindrome(999,99,-1)
