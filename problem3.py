76256c01eb0f87e680b2aeb9ec3f337ea374daf5
Lilia Kotonii <l.kotonij@yahoo.com>
2019-10-06 20:47:33 +0300

    problem3

---
+++ problem3.py
@@ -0,0 +1,10 @@
+#What is the largest prime factor of the number 600851475143 ?
+def largest_prime_factor(number):
+    i=2
+    while i*i<number:
+        while number%i==0:
+            number=number/i
+        i=i+1
+    return number
+
+print(largest_prime_factor(600851475143))
